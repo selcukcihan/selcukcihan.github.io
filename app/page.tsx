264f92a227ff5b4caeffc6b47f3753156e9fbff0
Selcuk Cihan <selcukcihan@gmail.com>
2024-05-08 17:56:05 +0300

    First phase

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,65 @@
 import Image from "next/image";
+import Link from 'next/link'
+import { SocialIcon } from 'react-social-icons'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="p-4 lg:p-8 min-h-screen">
+      <div className="py-1 lg:py-8 flex justify-center items-center">
+        <Link className="px-4 lg:px-8 underline" href="/">Home</Link>
+        <Link className="px-4 lg:px-8" href="/career">Career</Link>
+        <Link className="px-4 lg:px-8"href="/education">Education</Link>
+        <Link className="px-4 lg:px-8"href="/skills">Skills</Link>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3">
+        <div className="p-2 lg:p-4 flex flex-row lg:flex-col items-center space-x-4 lg:space-x-0">
+          <Image
+            src={`/profile.png`}
+            alt="Selçuk Cihan"
+            width={160}
+            height={160}
+            className="rounded-full selc-image-container relative top-2 lg:top-0" />
+          <div className="px-6 lg:px-0">
+            <p className="p-1 lg:p-2 text-2xl lg:text-4xl">Selçuk Cihan</p>
+            <div className="p-1 lg:p-2 lg:text-2xl italic font-extralight">
+              <p>Software Engineer</p>
+              <p>Freelancer</p>
+            </div>
+            <p className="p-1 lg:p-2 text-sm lg:text-xl font-extralight">selcukcihan@gmail.com</p>
+            <div className="space-x-1 p-1 lg:p-2 flex justify-center">
+              <span className="selc-icon-container"><SocialIcon url="https://github.com/selcukcihan" /></span>
+              <span className="selc-icon-container"><SocialIcon url="https://linkedin.com/in/selcukcihan" /></span>
+              <span className="selc-icon-container"><SocialIcon url="https://stackoverflow.com/users/4281182" /></span>
+              <span className="selc-icon-container hidden lg:inline"><SocialIcon url="https://twitter.com/scihan" /></span>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="hidden lg:block p-2 lg:py-16 lg:text-2xl font-extralight text-justify">
+          <p>As a former Amazonian devoted to helping customers and solving problems, Selçuk brings a strong sense of ownership and a track record of delivering results.</p>
+          <br/>
+          <p>With over seven years of experience developing applications on AWS, he has a proven ability to take on technical duties from operations to development and architecture.</p>
+        </div>
+        <div className="p-2 lg:p-4 flex flex-row lg:flex-col items-center justify-center">
+          <Image
+            src={`/architect.png`}
+            alt="AWS Solutions Architect Professional Certificate"
+            className="selc-image-container"
+            width={160}
+            height={160} />
+          <Image
+            src={`/developer.png`}
+            alt="AWS Developer Associate Certificate"
+            className="selc-image-container m-2 lg:m-8"
+            width={160}
+            height={160} />
+        </div>
       </div>
     </main>
   );
 }
+
+const testimonial = (text: string) => (
+  <div className="p-2 lg:p-4 lg:text-2xl font-extralight text-justify ">
+    <p>{text}</p>
+  </div>
+)
--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/selcukcihan.github.io",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH, // FOR PROD: "/selcukcihan.github.io",
 
   /**
    * Disable server-based image optimization. Next.js does not support
