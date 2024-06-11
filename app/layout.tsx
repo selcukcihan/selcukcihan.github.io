7bbce7ee23f4cdabb2605742bad6595c81681348
Selcuk Cihan <selcukcihan@gmail.com>
2024-06-11 18:41:06 +0300

    Add opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,69 @@
-import type { Metadata } from "next";
-import { Raleway } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Raleway } from "next/font/google"
+import "./globals.css"
 
-const font = Raleway({ subsets: ["latin"] });
+const font = Raleway({ subsets: ["latin"] })
 
-export const metadata: Metadata = {
-  title: "Selçuk Cihan | Freelance Software Engineer",
-  description: "This is the portfolio page of Selçuk Cihan, software engineer.",
-  keywords: [
-    "selcuk",
-    "cihan",
-    "software",
-    "engineer",
-    "freelancer",
-    "portfolio",
-    "developer",
-    "senior",
-    "resume",
-    "selcuk cihan",
-    "selçuk cihan",
-    "selcukcihan",
-    "cloud",
-    "aws",
-    "serverless",
-    "amazon",
-    "architect",
-    "technologist",
-    "fullstack",
-    "web development",
-    "python",
-    "javascript",
-    "java",
-    "scala",
-    "contractor"
-  ]
-};
+const title = "Selçuk Cihan | Freelance Software Engineer"
+const description = "This is the portfolio page of Selçuk Cihan, software engineer."
+
+const images = [{
+  url: `https://selcukcihan.com/profile.png`,
+  width: 360,
+  height: 360,
+  alt: title,
+}]
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: description,
+    description: title,
+    applicationName: title,
+    openGraph: {
+      siteName: title,
+      title: description,
+      description: title,
+      type: 'website',
+      url: 'https://selcukcihan.com',
+      images,
+    },
+    twitter: {
+      site: '@scihan',
+      creator: '@scihan',
+      card: 'summary',
+      title: description,
+      description: title,
+      images,
+    },
+    keywords: [
+      "selcuk",
+      "cihan",
+      "software",
+      "engineer",
+      "freelancer",
+      "portfolio",
+      "developer",
+      "senior",
+      "resume",
+      "selcuk cihan",
+      "selçuk cihan",
+      "selcukcihan",
+      "cloud",
+      "aws",
+      "serverless",
+      "amazon",
+      "architect",
+      "technologist",
+      "fullstack",
+      "web development",
+      "python",
+      "javascript",
+      "java",
+      "scala",
+      "contractor"
+    ],
+  }
+}
 
 export default function RootLayout({
   children,
@@ -45,5 +74,5 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>{children}</body>
     </html>
-  );
+  )
 }
