e2209711ae30ad409c01661916c1d7344237aca5
Selcuk Cihan <selcukcihan@gmail.com>
2024-06-21 12:20:56 +0300

    BG white

--- app/common.ts
+++ app/common.ts
@@ -1,3 +1,3 @@
-export const MainStyle = "min-h-screen dark:text-white bg-gray-50 dark:bg-gray-600"
+export const MainStyle = "min-h-screen dark:text-white bg-white dark:bg-gray-600"
 
 export const ContentStyle = "px-4 lg:px-8"
