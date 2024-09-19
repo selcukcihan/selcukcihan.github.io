fcdf928e131178a3b292b04454ea7bfa3da5812b
Selcuk Cihan <selcukcihan@gmail.com>
2024-09-19 18:18:26 +0300

    Add substack link

--- app/page.tsx
+++ app/page.tsx
@@ -72,6 +72,7 @@ const LargeHero = () => (
       <a href="https://linkedin.com/in/selcukcihan" target="#blank"><LinkedinIcon className={iconStyle}/></a>
       <a href="https://stackoverflow.com/users/4281182" target="#blank"><StackoverflowIcon className={iconStyle}/></a>
       <a href="https://twitter.com/scihan" target="#blank"><TwitterIcon className={iconStyle}/></a>
+      <a href="https://scihan.substack.com" target="#blank"><SubstackIcon className={iconStyle}/></a>
     </div>
   </div>
 )
@@ -153,4 +154,11 @@ const TwitterIcon = (props: any) => (
 
 const StackoverflowIcon = (props: any) => (
   <svg {...props} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Stack Overflow</title><path d="M15.725 0l-1.72 1.277 6.39 8.588 1.716-1.277L15.725 0zm-3.94 3.418l-1.369 1.644 8.225 6.85 1.369-1.644-8.225-6.85zm-3.15 4.465l-.905 1.94 9.702 4.517.904-1.94-9.701-4.517zm-1.85 4.86l-.44 2.093 10.473 2.201.44-2.092-10.473-2.203zM1.89 15.47V24h19.19v-8.53h-2.133v6.397H4.021v-6.396H1.89zm4.265 2.133v2.13h10.66v-2.13H6.154Z"/></svg>
+)
+
+const SubstackIcon = (props: any) => (
+  <svg {...props} viewBox="0 0 16 16">
+    <title>Substack</title>
+    <path d="M15 3.604H1v1.891h14v-1.89ZM1 7.208V16l7-3.926L15 16V7.208zM15 0H1v1.89h14z"/>
+  </svg>
 )
